9dea5fb6f4b17a0f255d53e5a0876de2be7bcf80
Abhay <26abhaykumarsingh@gmail.com>
2026-06-18 16:24:29 +0530

    List Properties on front page

---
+++ app/components/properties/PropertyList.tsx
@@ -0,0 +1,22 @@
+import PropertyListItem from "@/app/components/properties/PropertyListItem"
+
+const PropertyList = () => {
+  return (
+    <>
+      <PropertyListItem />
+      <PropertyListItem />
+      <PropertyListItem />
+      <PropertyListItem />
+      <PropertyListItem />
+      <PropertyListItem />
+      <PropertyListItem />
+      <PropertyListItem />
+      <PropertyListItem />
+      <PropertyListItem />
+      <PropertyListItem />
+      <PropertyListItem />
+    </>
+  )
+}
+
+export default PropertyList
