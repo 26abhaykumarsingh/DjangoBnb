14266c685da6088788d0848a68fff2d80bb28e1c
Abhay <26abhaykumarsingh@gmail.com>
2026-09-10 15:49:13 +0530

    Allow marking properties as favorite

--- app/components/properties/PropertyListItem.tsx
+++ app/components/properties/PropertyListItem.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image"
 import { PropertyType } from "@/app/components/properties/PropertyList"
 import { useRouter } from "next/navigation"
+import FavoriteButton from "../FavoriteButton";
 
 interface PropertyProps {
-  property: PropertyType
+  property: PropertyType,
+  markFavorite?: (is_favorite: boolean) => void;
 }
 
-const PropertyListItem = ({ property }: PropertyProps) => {
+const PropertyListItem = ({ property, markFavorite }: PropertyProps) => {
   const router = useRouter();
 
   return (
@@ -19,6 +21,14 @@ const PropertyListItem = ({ property }: PropertyProps) => {
           className="hover:scale-110 object-cover transition h-full w-full"
           alt="Beach House"
         />
+
+        {markFavorite && (
+          <FavoriteButton
+            id={property.id}
+            is_favorite={property.is_favorite}
+            mark_favorite={(is_favorite) => markFavorite(is_favorite)}
+          />
+        )}
       </div>
 
       <div className="mt-2">
